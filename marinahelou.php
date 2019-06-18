05413bd027903db574233bbe752daf6718805aba
Pedro Markun <pedro@markun.com.br>
2019-06-18 17:11:02 -0300

    votacao importada do emandato

--- marinahelou.php
+++ marinahelou.php
@@ -4,5 +4,28 @@ namespace Grav\Theme;
 class Marinahelou extends Deliver
 {
    // Some new methods, properties etc.
+
+   public function onTwigInitialized()
+        {
+          $this->grav['twig']->twig()->addFunction(
+          new \Twig_SimpleFunction('loadJson', [$this, 'getJson'])
+          );
+        }
+
+   public function getJson($url)
+        {
+            if ($url) {
+              $ch = curl_init();
+              curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              curl_setopt($ch, CURLOPT_URL,$url);
+              $result=curl_exec($ch);
+              curl_close($ch);
+            return json_decode($result);
+          }
+          else {
+            return null;
+          }
+        }
 }
 ?>
